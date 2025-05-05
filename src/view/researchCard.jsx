6b8f9425cef84a7d56719d683c7b9b0e2b074c2a
IsaacB <iwbcheetah@gmail.com>
2025-05-05 17:43:10 -0600

    Research header

--- src/view/researchCard.jsx
+++ src/view/researchCard.jsx
@@ -2,7 +2,7 @@
  * ContactsCard component. This component renders a card displaying a list of contacts.
  * It includes a header, a button to add a new contact, and a MapComponent to visualize contacts.
  */
-import { MapComponent, } from "flinntech";
+import { MapComponent } from "flinntech";
 import { PopupButton } from "flinntech";
 import { BaseComponent } from "flinntech";
 import ResearchCustomItem from "./components/researchCustomItem";
@@ -12,195 +12,186 @@ import ResearchCustomItem from "./components/researchCustomItem";
  * @extends BaseComponent
  */
 export default class ResearchCard extends BaseComponent {
-    /**
-     * Constructor for the ContactsCard component.
-     * Initializes component state and properties.
-     * @param {Object} props - Properties passed to the component.
-     */
-    constructor(props) {
-        super(props);
-        this.state = {
-            ...this.state,
-            defaultClass: "fit",
-            title: "Contacts",
-        };
+  /**
+   * Constructor for the ContactsCard component.
+   * Initializes component state and properties.
+   * @param {Object} props - Properties passed to the component.
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...this.state,
+      defaultClass: "fit",
+      title: "Contacts",
+    };
+  }
+
+  filterFunc() {
+    let filterText = this.propsState.tags?.split(",") || "";
+    let contactList = this.componentList.getList("contact");
+    let newList = [];
+    if (filterText) {
+      for (let tag of filterText) {
+        let list = contactList.filter((obj) =>
+          obj.getJson().tags.includes(tag)
+        );
+        newList = [...newList, ...list];
+      }
+    } else {
+      newList = [...contactList];
     }
 
-    filterFunc() {
-        
-        let filterText = this.propsState.tags?.split(',')||"";
-        let contactList = this.componentList.getList("contact");
-        let newList = []
-        if(filterText){
-            for (let tag of filterText) {
-                let list = contactList.filter(obj => obj.getJson().tags.includes(tag))
-                newList = [...newList, ...list]
+    this.dispatch({ selectedContacts: newList });
+  }
+
+  /**
+   * Returns the inner content of the ContactsCard component.
+   * @returns {JSX.Element} The inner content of the card.
+   */
+  getInnerContent() {
+    return (
+      <div className="map-container">
+        <div className="top-nav-float">
+          <nav className="top-nav">
+            <div className="nav-left">
+              <div className="nav-icon">
+                <i className="fas fa-circle"></i>
+              </div>
+              <div className="nav-title">Research</div>
+            </div>
+
+            <div className="nav-right">
+              <div className="nav-icon">
+                <button className="btn">
+                  A to Z <i className="fa-solid fa-angle-down"></i>
+                </button>
+              </div>
+              <div className="nav-icon">
+                <button className="btn">
+                  <i className="fa-solid fa-filter"></i>
+                </button>
+              </div>
+            </div>
+          </nav>
+
+          <div className="search-container">
+            <div className="search-bar">
+              <i className="fas fa-search search-icon"></i>
+              <input
+                onChange={(e) => {
+                  this.dispatch({ tags: e.target.value });
+                }}
+                className="search-input"
+                placeholder="Search"
+              />
+            </div>
+          </div>
+
+          <div className="filter-nav"></div>
+        </div>
+
+        <div className="card-header">Customer Profiles</div>
+        {/* Header row for the contacts data table */}
+
+        {/* Scrollable section for the contacts data */}
+        <MapComponent
+          filterFunc={(o) => {
+            if (!this.propsState.tags) {
+              return true;
             }
-        }
-        else{
-            newList=[...contactList];
-        }
-        
-        this.dispatch({ selectedContacts: newList })
+            let filterText = this.propsState.tags.split(",");
+            for (let tag of filterText) {
+              if (tag === "") {
+                continue;
+              }
+              if (o.getJson().tags.includes(tag)) {
+                return true;
+              }
+            }
 
-    }
+            return false;
+          }}
+          name="research"
+          mapContainerclassName="contact-list"
+          mapSectionclassName="contact"
+          cells={[
+            {
+              type: "custom",
+              hasLink: true,
+              custom: ResearchCustomItem,
+            },
+            // {
+            //     type: "custom",
+            //     custom: CheckIt,
+            //     check: (obj) => {
 
+            //         let contacts = this.propsState.selectedContacts ? [...this.propsState.selectedContacts] : [];
+            //         contacts.push(obj);
+            //         this.dispatch({ selectedContacts: contacts });
+            //     },
+            //     uncheck: (obj) => {
+            //         let contacts = this.propsState.selectedContacts ? [...this.propsState.selectedContacts] : [];
+            //         contacts = contacts.filter(contact => contact !== obj);
+            //         this.dispatch({ selectedContacts: contacts });
+            //     }
+            // },
 
-
-    /**
-     * Returns the inner content of the ContactsCard component.
-     * @returns {JSX.Element} The inner content of the card.
-     */
-    getInnerContent() {
-        
-        return (
-            <div className="map-container">
-                <div className="top-nav-float">
-                    <nav className="top-nav">
-                        <div className="nav-left">
-                            <div className="nav-icon">
-                                <i className="fas fa-circle"></i>
-                            </div>
-                            <div className="nav-title">Research</div>
-                        </div>
-
-                        <div className="nav-right">
-                            <div className="nav-icon">
-                                <button className="btn">A to Z <i className="fa-solid fa-angle-down"></i></button>
-                            </div>
-                            <div className="nav-icon">
-                                <button className="btn"><i className="fa-solid fa-filter"></i></button>
-                            </div>
-                        </div>
-
-
-                    </nav>
-
-
-                    <div className="search-container">
-                        <div className="search-bar">
-                            <i className="fas fa-search search-icon"></i>
-                            <input onChange={(e) => {
-                                this.dispatch({ tags: e.target.value })
-                            }}
-                            className="search-input" placeholder="Search" />
-                        </div>
-                    </div>
-
-                    <div className="filter-nav">
-                       
-
-                       
-                    </div>
-
-                </div>
-
-
-                
-
-
-
-                {/* Header row for the contacts data table */}
-
-                {/* Scrollable section for the contacts data */}
-                <MapComponent
-
-                    filterFunc={(o) => {
-                        if (!this.propsState.tags) {
-                            return true
-                        }
-                        let filterText = this.propsState.tags.split(',');
-                        for (let tag of filterText) {
-                            if (tag === "") {
-                                continue;
-                            }
-                            if (o.getJson().tags.includes(tag)) {
-                                return true;
-                            }
-                        }
-
-                        return false
-
-
-                    }}
-
-                    name="research"
-                    mapContainerclassName="contact-list"
-                    mapSectionclassName="contact"
-                    cells={[
-                        {
-                            type: "custom",
-                            hasLink: true,
-                            custom: ResearchCustomItem
-                        }
-                        // {
-                        //     type: "custom",
-                        //     custom: CheckIt,
-                        //     check: (obj) => {
-
-                        //         let contacts = this.propsState.selectedContacts ? [...this.propsState.selectedContacts] : [];
-                        //         contacts.push(obj);
-                        //         this.dispatch({ selectedContacts: contacts });
-                        //     },
-                        //     uncheck: (obj) => {
-                        //         let contacts = this.propsState.selectedContacts ? [...this.propsState.selectedContacts] : [];
-                        //         contacts = contacts.filter(contact => contact !== obj);
-                        //         this.dispatch({ selectedContacts: contacts });
-                        //     }
-                        // },
-
-                        // {
-                        //     type: "attribute",
-                        //     name: "name",
-                        //     style: { cursor: "pointer" },
-                        //     itemClick: (obj) => {
-                        //         this.dispatch({ currentPopupComponent: obj, popupSwitch: "updateContact" });
-                        //     }
-                        // },
-                        // { type: "attribute", name: "email" },
-                        // { type: "attribute", name: "phone" },
-                        // { type: "attribute", name: "status" },
-                    ]}
-                    hasLink={true}
-
-                />
-                {this.propsState.selectedContacts?.length > 0 &&
-                    <div id="floating-select-set" className="floating-select-set">
-                        <button className="floating-select-btn">
-                            <span className="floating-select-btn-text">Export</span>
-                        </button>
-                        <button onClick={()=>{
-                            for(let contact of this.propsState.selectedContacts){
-                                contact.del();
-
-                            }
-                            this.dispatch({selectedContacts:[]})
-                        }}className="floating-select-btn" >
-                            <span className="floating-select-btn-text">Delete</span>
-                        </button>
-                        <button className="floating-select-btn floating-select-primary-btn">
-                            <PopupButton
-                    formclassName="FCImgButton"
-                    content={<span className="floating-select-btn-text">Add to Sequence</span>}
-                    popupSwitch="addToSequence"
-                />
-                            
-                        </button>
-                    </div>
+            // {
+            //     type: "attribute",
+            //     name: "name",
+            //     style: { cursor: "pointer" },
+            //     itemClick: (obj) => {
+            //         this.dispatch({ currentPopupComponent: obj, popupSwitch: "updateContact" });
+            //     }
+            // },
+            // { type: "attribute", name: "email" },
+            // { type: "attribute", name: "phone" },
+            // { type: "attribute", name: "status" },
+          ]}
+          hasLink={true}
+        />
+        {this.propsState.selectedContacts?.length > 0 && (
+          <div id="floating-select-set" className="floating-select-set">
+            <button className="floating-select-btn">
+              <span className="floating-select-btn-text">Export</span>
+            </button>
+            <button
+              onClick={() => {
+                for (let contact of this.propsState.selectedContacts) {
+                  contact.del();
                 }
-            </div>
-        );
-    }
+                this.dispatch({ selectedContacts: [] });
+              }}
+              className="floating-select-btn"
+            >
+              <span className="floating-select-btn-text">Delete</span>
+            </button>
+            <button className="floating-select-btn floating-select-primary-btn">
+              <PopupButton
+                formclassName="FCImgButton"
+                content={
+                  <span className="floating-select-btn-text">
+                    Add to Sequence
+                  </span>
+                }
+                popupSwitch="addToSequence"
+              />
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
 
-    /**
-     * Renders the ContactsCard component.
-     * @returns {JSX.Element} The rendered component.
-     */
-    render() {
-        return (
-            <div className={this.props.pageClass || this.state.defaultClass} >
-                {this.getInnerContent()}
-            </div>
-        );
-    }
+  /**
+   * Renders the ContactsCard component.
+   * @returns {JSX.Element} The rendered component.
+   */
+  render() {
+    return (
+      <div className={this.props.pageClass || this.state.defaultClass}>
+        {this.getInnerContent()}
+      </div>
+    );
+  }
 }
